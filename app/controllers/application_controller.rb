bedd73a956120c43a596ff90e517587aa27d46d0
walterbm <walter.beller.morales@gmail.com>
2015-09-09 02:26:33 -0400

    add elected official option to citizen registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :public_key, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :elected_official, :public_key, :password, :password_confirmation) }
     end
 end
